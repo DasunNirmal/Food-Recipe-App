e122bf927153403ef675bb8b8ce646b93750cd2e
DasunNirmal <dasun200301@gmail.com>
2025-02-27 01:10:46 +0530

    dummy

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,16 +1,24 @@
 import {Button, Text, View} from "react-native";
 import {SafeAreaView} from "react-native-safe-area-context";
 import {ParamListBase, useNavigation} from "@react-navigation/native";
-import {NativeStackNavigationProp, NativeStackNavigatorProps} from "@react-navigation/native-stack";
+import {NativeStackNavigationProp} from "@react-navigation/native-stack";
+import {useEffect} from "react";
 
 export const HomeScreen = () => {
 
     const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
+
+    useEffect(() => {
+        navigation.setOptions({
+            navigationBarColor: 'white',
+        });
+    }, [navigation]);
+
     return (
         <SafeAreaView>
             <View>
                 <Text>Home</Text>
-                <Button title="Go to Restaurant" onPress={() => navigation.navigate('Restaurant')} />
+                <Button title="Go to Welcome Page" onPress={() => navigation.navigate('Welcome')} />
             </View>
         </SafeAreaView>
     );
