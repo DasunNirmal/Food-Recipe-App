897ba67d8bb7aa4c550f33a54bd7fc4c0c7054f2
DasunNirmal <dasun200301@gmail.com>
2025-02-27 01:11:16 +0530

    done

--- Navigation.tsx
+++ Navigation.tsx
@@ -1,16 +1,17 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import {HomeScreen} from "./screens/HomeScreen";
-import {RestaurantScreen} from "./screens/RestaurantScreen";
+import {WelcomeScreen} from "./screens/WelcomeScreen";
 
 const Stack = createNativeStackNavigator();
 
 export const Navigation =() => {
+    const backgroundColor = 'rgb(254 205 211)';
     return (
         <NavigationContainer>
-            <Stack.Navigator screenOptions={{headerShown: false}}>
+            <Stack.Navigator initialRouteName={'Welcome'} screenOptions={{headerShown: false, navigationBarColor: backgroundColor}}>
+                <Stack.Screen name="Welcome" component={WelcomeScreen} />
                 <Stack.Screen name="Home" component={HomeScreen} />
-                <Stack.Screen name="Restaurant" component={RestaurantScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     );
